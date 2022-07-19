780828df2a107ac13f654f52a1ce1c8a0bd5df6c
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2022-07-20 01:43:23 +0200

    adding workign

--- tables/models.py
+++ tables/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Table(models.Model):
-    pass
+    lng = models.FloatField()
+    lat = models.FloatField()
+    image = models.CharField(max_length=300)
--- tables/views.py
+++ tables/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Table
 
 def index(request):
     # return HttpResponse("Hello  ")
-    return render(request, "tables/index.html", {"coordinates": [["40.463667", "39.399872"], ["39.399872", "-8.224454"]]})
+    if request.method == "POST":
+        print(request.POST)
+        Table.objects.create(lat=request.POST["lat"], lng=request.POST["lng"])
+        print(redirect('/', permanent=True))
+        return redirect('/', permanent=True) # To avoid inserting the same item twice when doing F5
+
+    print(Table.objects.all())
+    return render(request, "tables/index.html", {"coordinates": Table.objects.all()})
+def add(request):
+    return render(request, "tables/add.html")
