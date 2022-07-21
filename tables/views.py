2e5b8cc7695fa8dfe613f86b459066275426ca2f
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2022-07-22 01:28:21 +0200

    added a basic search

--- tables/views.py
+++ tables/views.py
@@ -5,11 +5,24 @@ from .models import Table
 def index(request):
     # return HttpResponse("Hello  ")
     if request.method == "POST":
-        print(request.POST)
-        Table.objects.create(lat=request.POST["lat"], lng=request.POST["lng"])
-        return redirect('/', permanent=True) # To avoid inserting the same item twice when doing F5
+        lat, lng = float(request.POST["lat"]), float(request.POST["lng"])
+        if request.POST["type"] == "add": # Add new marker
+            Table.objects.create(lat=lat, lng=lng)
+            return redirect('/', permanent=True) # To avoid inserting the same item twice when doing F5
+        else: # Search in readius of marker
+            radius = float(request.POST["radius"]) / 111.0 # Terrible longitude/latitude to km
+            print(radius, type(radius))
+            tables = Table.objects.all().filter(lng__lt=lng+radius, 
+                                                lng__gt=lng-radius, 
+                                                lat__lt=lat+radius,
+                                                lat__gt=lat-radius)
+    else:
+        tables = Table.objects.all()
 
-    print(Table.objects.all())
-    return render(request, "tables/index.html", {"coordinates": Table.objects.all()})
+    return render(request, "tables/index.html", {"coordinates": tables})
+
 def add(request):
     return render(request, "tables/add.html")
+
+def search(request):
+    return render(request, "tables/search.html")
